f9614b4d5ac2e17b861dc5593f4fad8c48c58aef
c19719949 <57361393+c19719949@users.noreply.github.com>
2022-11-06 20:24:26 +0000

    Add files via upload

--- Lab2/Problem2/script2.js
+++ Lab2/Problem2/script2.js
@@ -11,10 +11,26 @@ async function getdata()
     const data2 = data.filter(entry => entry.title.split(' ').length > 6);
     
     console.log(data2);
+    let allBodies = ""
 
 
+    data.forEach((entry)  =>{
 
-
+        allBodies = allBodies.concat(entry.body.toString());
+    
+    
+    });
+    
+        var words = allBodies.replace(/[.]/g, '').split(/\s/);
+        var frequency = {};
+        words.forEach(function(w) {
+        if (!frequency[w]) {
+            frequency[w] = 0;
+        }
+        frequency[w] += 1;
+        })
+    console.log(frequency)
+ 
 }
 
 getdata();
